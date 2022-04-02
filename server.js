705d2af393f3310d835942a2a6ad74706fe265ee
Mateus Vieira <68292695+MateusVrs@users.noreply.github.com>
2022-04-02 18:31:04 -0300

    server express

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const { resolve } = require('path')
+const app = express()
+
+app.use('/',
+    express.static(
+        resolve(
+            __dirname,
+            './build'
+        )
+    )
+)
+
+app
+    .listen(process.env.PORT || 3000, (err) => {
+        if (err) { return console.log(err) }
+
+        console.log('Tudo funcionando certinho')
+    })
