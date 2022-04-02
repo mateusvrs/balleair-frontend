c8dcb56a2cef929b6d3e975d3178e940a0cecf03
Mateus Vieira <68292695+MateusVrs@users.noreply.github.com>
2022-04-02 18:04:55 -0300

    base config

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
-function App() {
+import { PrivateRoute } from "./components/PrivateRoute"
+import { PublicRoute } from "./components/PublicRoute"
+import { TravelerOnlyRoute } from "./components/TravelerOnlyRoute"
+
+import { AuthContextProvider } from "./contexts/AuthContext"
+import { LoadingContextProvider } from "./contexts/LoadingContext"
+import { ModalOpenContextProvider } from "./contexts/ModalOpenContext"
+import { SelectedFlightContextProvider } from "./contexts/SelectedFlightContext"
+
+import { FlightsPage } from "./pages/Flights"
+import { HomePage } from "./pages/Home"
+import { LoginPage } from "./pages/Login"
+import { RegisterPage } from "./pages/Register"
+
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ModalOpenContextProvider>
+      <SelectedFlightContextProvider>
+        <LoadingContextProvider>
+          <AuthContextProvider>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<PublicRoute component={<RegisterPage />} />} />
+                <Route path="/login" element={<PublicRoute component={<LoginPage />} />} />
+                <Route path="/home" element={<TravelerOnlyRoute component={<HomePage />} />} />
+                <Route path="/flights" element={<PrivateRoute component={<FlightsPage />} />} />
+              </Routes>
+            </BrowserRouter>
+          </AuthContextProvider>
+        </LoadingContextProvider>
+      </SelectedFlightContextProvider>
+    </ModalOpenContextProvider>
+  )
 }
-
-export default App;
